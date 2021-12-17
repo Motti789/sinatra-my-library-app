6f61dfe8f5e2a672de78eeda8d6e223c80b2064e
Motti789 <mchillywilly@gmail.com>
2021-12-17 13:25:19 -0500

    enabled sessions, created a controller post route for login and the view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secrect, "our_library_app"
   end
 
   get "/" do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,27 @@ class UsersController < ApplicationController
       erb :login
     end
 
+     # receive the login form, find the user, and log him in.
+    post '/login' do
+    @user = User.find_by(email: params[:email])
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id # Here's where the login happens
+      redirect "users/#{@user.id}"
+
+
+
+
+
+    else
+     end
+    end
+
     # render the signup form
     get '/signup' do
 
+    end
+
+    get '/users/:id' do     #dynamic route which will change based off the user
+      "This is the users route"
     end
 end
