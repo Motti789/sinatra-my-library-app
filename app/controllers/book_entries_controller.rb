1291b7ce2f5acf569edbfac8cdf4785ffa86ffde
Motti789 <mchillywilly@gmail.com>
2021-12-22 11:05:20 -0500

    added a edit route and controller

--- app/controllers/book_entries_controller.rb
+++ app/controllers/book_entries_controller.rb
@@ -1,8 +1,34 @@
 class BookEntriesController < ApplicationController
 
     get '/book_entries/new' do
+        if logged_in?
       erb :'/book_entries/new'
+        else
+         redirect '/login'
+        end
     end
 
+    post '/book_entries' do
+        if !logged_in?
+            redirect '/'
+        end
+        if params[:title] !="" && params[:author] != "" && params[:genre] != ""
+         @book_entry = BookEntry.create(title: params[:title], author: params[:author], genre: params[:genre],
+         user_id: current_user.id)
+          redirect "/book_entries/#{@book_entry.id}"
+        else
+         redirect '/book_entries/new'
+        end
+    end
+
+    get '/book_entries/:id' do
+      @book_entry = BookEntry.find(params[:id])
+      erb :'/book_entries/show'
+    end
+
+    get '/book_entries/:id/edit' do
+        
+     erb :'/book_entries/edit'
+    end
     
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,11 @@ class UsersController < ApplicationController
      # receive the login form, find the user, and log him in.
     post '/login' do
     @user = User.find_by(email: params[:email])
-    if @user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id # Here's where the login happens
-      puts session
-      redirect "users/#{@user.id}"
+      redirect to "/users/#{@user.id}"
     else
-     redirect '/signup'
+     redirect to '/login'
      end
     end
 
